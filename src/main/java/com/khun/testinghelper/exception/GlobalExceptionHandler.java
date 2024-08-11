99e5f4fb7563824ffdcb9234406358a635595993
Khun Aung Paing <54024040+khunaungpaing@users.noreply.github.com>
2024-08-12 00:12:07 +0630

    create global exception handler and remove thymeleaf

--- src/main/java/com/khun/testinghelper/exception/GlobalExceptionHandler.java
+++ src/main/java/com/khun/testinghelper/exception/GlobalExceptionHandler.java
@@ -1,18 +1,41 @@
 package com.khun.testinghelper.exception;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import com.khun.testinghelper.domain.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public String handleResourceNotFoundException(ResourceNotFoundException ex) {
-        return "404";
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
+        var response = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "bad request",
+                e.getMessage()
+        );
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(Exception.class)
-    public String handleException(Exception ex) {
-        return "500";
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e) {
+        var response = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "not found",
+                e.getMessage()
+        );
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ResourceConflictException.class)
+    public ResponseEntity<ErrorResponse> handleResourceConflictException(ResourceConflictException e) {
+        var response = new ErrorResponse(
+                HttpStatus.CONFLICT.value(),
+                "conflict",
+                e.getMessage()
+        );
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 }
